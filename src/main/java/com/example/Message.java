ef91e9cd4262dc67a8ca39c5cf7598ecc9b2b440
Saurabh Agrawal <asaurabh@Sourabhs-MacBook-Pro.local>
2016-12-30 12:21:09 +0530

    Initial commit.

---
+++ src/main/java/com/example/Message.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class Message {
+
+    public static final String MESSAGE = "Hello World";
+
+    public static String getMessage() {
+        return MESSAGE;
+    }
+}
