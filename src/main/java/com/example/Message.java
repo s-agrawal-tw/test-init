a1c41aa23bd0df1a58b91c73a5a116cf4d838f1a
Saurabh Agrawal <asaurabh@Sourabhs-MacBook-Pro.local>
2016-12-30 12:27:32 +0530

    Refactored Message class.

--- src/main/java/com/example/Message.java
+++ src/main/java/com/example/Message.java
@@ -3,12 +3,13 @@ package com.example;
 public class Message {
 
     public static final String MESSAGE = "Hello World";
+    public static final String EXCLAMATION_MARK = "!!!";
 
     public static String getMessage() {
         return MESSAGE;
     }
 
     public static String getFormattedMessage() {
-        return getMessage().concat("!!!");
+        return getMessage().concat(EXCLAMATION_MARK);
     }
 }
